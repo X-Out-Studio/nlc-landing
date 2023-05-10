3ea18b58edb9113c0396789749f5698d80d5e56b
katyusya <katusha.fedyakena@mail.ru>
2023-05-10 18:03:43 +0300

    add steps on stageWork

--- components/Index/stagesWork.vue
+++ components/Index/stagesWork.vue
@@ -6,7 +6,27 @@
         <p class="stagesWork__title">этапы работы</p>
         <div class="stagesWork__steps">
             <div class="stagesWork__arrow">
-                <img src="../../assets/img/stagesWork/arrow.png" alt="">
+                <div class="stagesWork__step">
+                    <div class="stagesWork__step--content">
+                        <p class="stagesWork__step--name">1. подбор автомобиля</p>
+                        <p class="stagesWork__step--text">Специалисты NLC подбирают вам авто по необходимым параметрам на
+                            площадках Европы.</p>
+                    </div>
+                    <div class="stagesWork__step--done">
+                        <img src="../../assets/img/stagesWork/done.svg" alt="">
+                    </div>
+                </div>
+                <div class="stagesWork__step stagesWork__step--right">
+                    <div class="stagesWork__step--content">
+                        <p class="stagesWork__step--name">2. заключение договора</p>
+                        <p class="stagesWork__step--text">Компания NLC заключает договоры с клиентами на поставку автомобиля
+                            как по полной, так и по частичной оплате (мин. аванс - 25%).</p>
+                    </div>
+                    <div class="stagesWork__step--done">
+                        <img src="../../assets/img/stagesWork/done.svg" alt="">
+                    </div>
+                </div>
+                <img class="stagesWork__arrow-img" src="../../assets/img/stagesWork/arrow.png" alt="">
             </div>
         </div>
         <p class="stagesWork__text">
@@ -21,7 +41,7 @@
             <p class="stagesWork__weeks-text">2 недели</p>
             <Button />
         </div>
-        <div class="stagesWork__weeks--car">
+        <div class="stagesWork__car">
             <img src="../../assets/img/stagesWork/car.png" alt="">
         </div>
     </section>
@@ -29,7 +49,6 @@
 
 <style lang="scss" scoped>
 .stagesWork {
-    position: relative;
     background-color: var(--blackLight);
     padding-top: 40px;
     padding-bottom: 37px;
@@ -61,8 +80,71 @@
         }
     }
 
+    &__steps {
+        margin-top: 13px;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+
+    &__step {
+        display: flex;
+        flex-direction: row;
+        gap: 6px;
+        align-items: center;
+
+        &--content {
+            background-color: #1E1E1E;
+            border: 1px dashed #C6F3B6;
+            border-radius: 7px;
+            padding: 18px 20px 15px 35px;
+        }
+
+        &--name {
+            font-weight: 700;
+            font-size: 16px;
+            line-height: 128.61%;
+            text-transform: uppercase;
+            color: #EDEDED;
+        }
+
+        &--text {
+            margin-top: 27px;
+            font-weight: 400;
+            font-size: 14px;
+            line-height: 147.11%;
+            color: #EDEDED;
+        }
+
+        &--done {
+            background-color: #C6F3B6;
+            border: 8px solid #D9FFCC;
+            border-radius: 50%;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: center;
+            width: 33px;
+            height: 33px;
+        }
+
+        &--right {
+            .stagesWork__step--done {
+                order: -1;
+            }
+        }
+    }
+
     &__arrow {
-        display: grid;
+        position: relative;
+        display: flex;
+        flex-direction: column;
+
+
+        &-img {
+            position: absolute;
+            left: 50%;
+        }
     }
 
     &__weeks {
@@ -81,12 +163,13 @@
             text-transform: uppercase;
             font-family: 'Druk Text Wide Cyr';
         }
+    }
 
-        &--car {
-            top: 206px;
-            left: 0;
-            position: absolute;
-        }
+    &__car {
+        top: 206px;
+        left: 0;
+        position: absolute;
+        z-index: 0;
     }
 
     .button {
