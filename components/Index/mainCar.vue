0a10c69bb5322ac00d2ffc7ef11ad743bb0b53eb
Denis Demin <denis.demins@gmail.com>
2023-05-10 18:03:22 +0300

    dev main component

--- components/Index/mainCar.vue
+++ components/Index/mainCar.vue
@@ -1,36 +1,38 @@
 <template>
   <div class="main-content">
-    <div class="img-container">
-      <div class="title">Автомобили из Европы под заказ</div>
-      <img src="../../assets/img/index/mainCar/img.png" alt="" />
+    <div class="content-container">
+      <div class="title">Автомобили из Европы под&nbsp;заказ</div>
+      <div class="telegram-box"></div>
     </div>
+    <img src="../../assets/img/index/mainCar/img.png" alt="" />
   </div>
 </template>
 <style scoped lang='scss'>
 .main-content {
-  // .main-container {
-  //   margin: 118px 150px 64px 150px;
-  // }
-  .img-container {
-    width: 100%;
-    height: 967px;
-    position: relative;
-
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
+  width: 100%;
+  height: 967px;
+  position: relative;
+  .content-container {
+    margin: 118px 150px 64px 150px;
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     .title {
-      position: absolute;
+      max-width: 800px;
       font-weight: 900;
-      font-size: 79px;
+      font-size: 65px;
       line-height: 128.61%;
       text-transform: uppercase;
       color: #ffffff;
       font-family: "Druk Text Wide Cyr";
       font-style: normal;
     }
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
 }
 </style>
