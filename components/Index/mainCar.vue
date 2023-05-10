7b976866d15668322dc66e66c42e85141fe414e5
Denis Demin <denis.demins@gmail.com>
2023-05-10 16:18:44 +0300

    add fonts + main block

---
+++ components/Index/mainCar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="main-content">
+    <div class="img-container">
+      <div class="title">Автомобили из Европы под заказ</div>
+      <img src="../../assets/img/index/mainCar/img.png" alt="" />
+    </div>
+  </div>
+</template>
+<style scoped lang='scss'>
+.main-content {
+  // .main-container {
+  //   margin: 118px 150px 64px 150px;
+  // }
+  .img-container {
+    width: 100%;
+    height: 967px;
+    position: relative;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+    .title {
+      position: absolute;
+      font-weight: 900;
+      font-size: 79px;
+      line-height: 128.61%;
+      text-transform: uppercase;
+      color: #ffffff;
+      font-family: "Druk Text Wide Cyr";
+      font-style: normal;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,3 @@
 <template>
-  <h1>asdadsad</h1>
+  <IndexMainCar />
 </template>
