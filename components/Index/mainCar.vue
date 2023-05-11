2f6626d2c53bce9e52056c8bc0cb995a0c950f67
Denis Demin <denis.demins@gmail.com>
2023-05-11 12:16:30 +0300

    dev main component + swiperModels

--- components/Index/mainCar.vue
+++ components/Index/mainCar.vue
@@ -2,37 +2,82 @@
   <div class="main-content">
     <div class="content-container">
       <div class="title">Автомобили из Европы под&nbsp;заказ</div>
-      <div class="telegram-box"></div>
+      <div class="telegram-box">
+        <div class="text">
+          БЕСПЛАТНЫЙ ПОДБОР АВТО
+          <span>ЗА 2 ЧАСА</span>
+        </div>
+        <a class="telegram-button" href="https://ya.ru" target="_blank">
+          <svg
+            width="66"
+            height="14"
+            viewBox="0 0 66 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M16.3525 0.0920533L1.2938 5.79416C0.266098 6.19948 0.272044 6.76244 1.10524 7.01347L4.97142 8.19776L13.9166 2.65579C14.3396 2.40308 14.726 2.53903 14.4084 2.81592L7.16101 9.2386L6.89432 13.1526C7.28501 13.1526 7.45743 12.9766 7.67656 12.769L9.55444 10.9758L13.4605 13.809C14.1808 14.1984 14.698 13.9983 14.8772 13.1543L17.4414 1.28802C17.7038 0.254685 17.0396 -0.213193 16.3525 0.0920533Z"
+              fill="white"
+            />
+            <path
+              d="M65.8536 7.35355C66.0488 7.15829 66.0488 6.84171 65.8536 6.64645L62.6716 3.46447C62.4763 3.2692 62.1597 3.2692 61.9645 3.46447C61.7692 3.65973 61.7692 3.97631 61.9645 4.17157L64.7929 7L61.9645 9.82843C61.7692 10.0237 61.7692 10.3403 61.9645 10.5355C62.1597 10.7308 62.4763 10.7308 62.6716 10.5355L65.8536 7.35355ZM37.5 7.5H65.5V6.5H37.5V7.5Z"
+              fill="white"
+            />
+          </svg>
+        </a>
+      </div>
     </div>
-    <img src="../../assets/img/index/mainCar/img.png" alt="" />
   </div>
 </template>
 <style scoped lang='scss'>
 .main-content {
+  display: flex;
   width: 100%;
   height: 967px;
-  position: relative;
+  background-image: url("../../assets/img/index/mainCar/img.png");
+  padding: 118px 150px 64px 150px;
   .content-container {
-    margin: 118px 150px 64px 150px;
-    position: absolute;
     display: flex;
+    width: 100%;
+    height: 100%;
     flex-direction: column;
     justify-content: space-between;
     .title {
       max-width: 800px;
       font-weight: 900;
-      font-size: 65px;
+      font-size: 79px;
       line-height: 128.61%;
       text-transform: uppercase;
       color: #ffffff;
       font-family: "Druk Text Wide Cyr";
       font-style: normal;
     }
-  }
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
+    .telegram-box {
+      align-self: flex-end;
+      background: rgba(23, 23, 23, 0.56);
+      backdrop-filter: blur(17px);
+      border-radius: 13px;
+      display: flex;
+      align-items: center;
+      padding: 34px 59px 44px 47px;
+      gap: 46px;
+      color: white;
+      font-weight: 700;
+      font-size: 30px;
+      line-height: 156.61%;
+      text-transform: uppercase;
+      .text {
+        max-width: 340px;
+        span {
+          color: #29aee4;
+        }
+      }
+      .telegram-button {
+        padding: 10px 44px;
+        border: 1px solid #ffffff;
+        border-radius: 108px;
+      }
+    }
   }
 }
 </style>
---
+++ components/Index/swiperModels.vue
@@ -0,0 +1,136 @@
+<script setup>
+const navigateParams = {
+  nextEl: ".swiper-next",
+  prevEl: ".swiper-prev",
+};
+</script>
+
+<template>
+  <div class="swiper-section">
+    <div class="title-container">
+      <div class="global-title">Дилеры, с которыми мы работаем</div>
+    </div>
+    <div class="swiper-container">
+      <Swiper
+        :slides-per-view="4.5"
+        :spaceBetween="'42'"
+        :modules="[SwiperNavigation]"
+        :navigation="navigateParams"
+        :autoplay="{
+          delay: 1000,
+        }"
+      >
+        <SwiperSlide>
+          <div class="slide-content">
+            <img src="../../assets/img/swiperModels/merc.png" alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div class="slide-content">
+            <img src="../../assets/img/swiperModels/bmw.png" alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div class="slide-content">
+            <img src="../../assets/img/swiperModels/tesla.png" alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div class="slide-content">
+            <img src="../../assets/img/swiperModels/tesla.png" alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div class="slide-content">
+            <img src="../../assets/img/swiperModels/tesla.png" alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div class="slide-content">
+            <img src="../../assets/img/swiperModels/tesla.png" alt="" />
+          </div>
+        </SwiperSlide>
+      </Swiper>
+      <div class="swiper-navigate">
+        <div class="swiper-prev">
+          <svg
+            width="88"
+            height="88"
+            viewBox="0 0 88 88"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="44" cy="44" r="43.5" stroke="#35322E" />
+            <path
+              d="M58.5092 44.7391C58.7045 44.5438 58.7045 44.2272 58.5092 44.0319L55.3273 40.85C55.132 40.6547 54.8154 40.6547 54.6201 40.85C54.4249 41.0452 54.4249 41.3618 54.6201 41.5571L57.4486 44.3855L54.6201 47.2139C54.4249 47.4092 54.4249 47.7258 54.6201 47.921C54.8154 48.1163 55.132 48.1163 55.3273 47.921L58.5092 44.7391ZM29.8906 44.8855H58.1557V43.8855H29.8906V44.8855Z"
+              fill="#35322E"
+            />
+          </svg>
+        </div>
+        <div class="swiper-next">
+          <svg
+            width="88"
+            height="88"
+            viewBox="0 0 88 88"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="44" cy="44" r="43.5" stroke="#35322E" />
+            <path
+              d="M58.5092 44.7391C58.7045 44.5438 58.7045 44.2272 58.5092 44.0319L55.3273 40.85C55.132 40.6547 54.8154 40.6547 54.6201 40.85C54.4249 41.0452 54.4249 41.3618 54.6201 41.5571L57.4486 44.3855L54.6201 47.2139C54.4249 47.4092 54.4249 47.7258 54.6201 47.921C54.8154 48.1163 55.132 48.1163 55.3273 47.921L58.5092 44.7391ZM29.8906 44.8855H58.1557V43.8855H29.8906V44.8855Z"
+              fill="#35322E"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped lang='scss'>
+.swiper-section {
+  background: #f4f4f4;
+  padding: 57px 150px 61px 150px;
+}
+.title-container {
+  max-width: 480px;
+}
+.swiper-container {
+  margin: 91px 0 0 0;
+}
+.slide-content {
+  width: 143px;
+  height: 143px;
+  margin: 20px 92.5px;
+  img {
+    width: fit-content;
+    height: fit-content;
+    object-fit: cover;
+  }
+}
+.swiper-slide {
+  display: flex;
+  justify-content: center;
+  background: white;
+  box-shadow: 4px 14px 37px rgba(0, 0, 0, 0.03);
+  border-radius: 12px;
+}
+.swiper-navigate {
+  display: flex;
+  gap: 20px;
+  justify-content: flex-end;
+  margin: 52px 0 0 0;
+  .swiper-prev {
+    svg {
+      transform: rotate(180deg);
+    }
+  }
+  .swiper-prev,
+  .swiper-next {
+    cursor: pointer;
+  }
+  .swiper-button-disabled {
+    opacity: 0.24;
+    cursor: auto;
+  }
+}
+</style>
