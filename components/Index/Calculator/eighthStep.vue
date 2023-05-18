b04c0a66f178a55bcf47c3a9919babf52626846e
Denis Demin <denis.demins@gmail.com>
2023-05-18 14:16:55 +0300

    fix

--- components/Index/Calculator/eighthStep.vue
+++ components/Index/Calculator/eighthStep.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useCalcFormStore } from "@/store/calcForm";
+const runtimeConfig = useRuntimeConfig();
 const store = useCalcFormStore();
 const openForm = () => {
   store.openModal ? (store.openModal = false) : (store.openModal = true);
@@ -40,16 +41,32 @@ const checkForm = () => {
       </div>
       <div class="select-container final with-input">
         <div class="select-row eighth">
-          <input type="text" class="wishes-input" v-model="store.answers.fio" :class="errorFlag ? 'error' : ''"
-            placeholder="Имя Фамилия" />
-          <input type="text" class="wishes-input" :class="errorFlag ? 'error' : ''" v-model="store.answers.number"
-            placeholder="+7 ..." v-maska data-maska="+7 ### ###-##-##" required/>
+          <input
+            type="text"
+            class="wishes-input"
+            v-model="store.answers.fio"
+            :class="errorFlag ? 'error' : ''"
+            placeholder="Имя Фамилия"
+          />
+          <input
+            type="text"
+            class="wishes-input"
+            :class="errorFlag ? 'error' : ''"
+            v-model="store.answers.number"
+            placeholder="+7 ..."
+            v-maska
+            data-maska="+7 ### ###-##-##"
+            required
+          />
         </div>
       </div>
       <div class="navigate-container final">
         <button @click="checkForm">Отправить</button>
-        <span>Нажимая на кнопку «Отправить», вы соглашаетесь с условиями
-          <a href="/#" target="_blank">Политики обработки персональных данных</a>
+        <span
+          >Нажимая на кнопку «Отправить», вы соглашаетесь с условиями
+          <a href="/#" target="_blank"
+            >Политики обработки персональных данных</a
+          >
         </span>
       </div>
     </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,11 @@
 export default defineNuxtConfig({
   css: ["@/assets/styles/main.scss"],
   modules: ["nuxt-swiper", "@pinia/nuxt"],
+  runtimeConfig: {
+    handlBack: "http://localhost:7777/api/",
+    endpoints: {
+      captcha: "check-captcha/",
+      telegramSend: "telegram-send/",
+    },
+  },
 });
