a2fbaabe0f126290d796991492e7be9292372b3e
Denis Demin <denis.demins@gmail.com>
2023-05-25 16:45:01 +0300

    add metrika and change api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["@/assets/styles/main.scss"],
-  modules: ["nuxt-swiper", "@pinia/nuxt"],
+  modules: [
+    "nuxt-swiper",
+    "@pinia/nuxt",
+    [
+      "yandex-metrika-module-nuxt3",
+      {
+        id: "93635804",
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true,
+        webvisor: true,
+        ecommerce: "dataLayer",
+      },
+    ],
+  ],
   plugins: [{ src: "@/plugins/aos", ssr: false, mode: "client" }],
 });
